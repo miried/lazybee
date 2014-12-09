0354d150d1971e1d3a678b44d9bac48090aab17f
Michael Rieder <citizen@1984.com>
2014-12-10 00:20:38 +0100

    update

--- bspmap.cpp
+++ bspmap.cpp
@@ -16,11 +16,16 @@ void bspmap::open( const char* mname )
 	mapfile->read( &header, sizeof(header) );
 
 	if (memcmp(header.id,"2015",4)==0)
-		con_printf( "loading 2015 map\n" );
+		con_printf( "loading '2015' map\n" );
 	else {
 		con_printf( "unknown map format %c%c%c%c\n",
 			header.id[0],header.id[1],header.id[2],header.id[3] );
 	}
+	con_printf( "map is BSP v%.2i\n", header.version );
+	
+	if (header.version == 19)
+		con_printf( "format is MOHAA v1.12, checksum %#.8x\n",
+				header.checksum );
 	
 	mapfile->read( lumps, sizeof(lumps) );
 	
